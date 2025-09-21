7f4e96171c0415f3c7bfcfd727209ec7ef08e590
Eitan Mic <eitanm@me.com>
2025-09-21 14:06:20 +0300

    update

--- src/main.cc
+++ src/main.cc
@@ -141,14 +141,12 @@ int test_str() {
 
 int test_vector() {
 
-    // Create a vector containing integers
+
     std::vector<int, NoHeapAllocator<int>> v = {8, 4, 5, 9};
  
-    // Add two more integers to vector
     v.push_back(6);
     v.push_back(9);
  
-    // Overwrite element at position 2
     v[2] = -1;
  
     // Print out the vector
