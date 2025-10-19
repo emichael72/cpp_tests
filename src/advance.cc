e5dd3b702f249eae91007e458cbbd0424349679d
Eitan Michaelson <eitanm@me.com>
2025-10-19 10:08:31 +0300

    updates

--- src/advance.cc
+++ src/advance.cc
@@ -3,8 +3,28 @@
 
 #include <iostream>
 
+
+// primary template
+template <typename Tuple, typename... Ts>
+struct append;
+
+
+// partial specialization for std::tuple<...>
+template <typename... Us, typename... Ts>
+struct append<std::tuple<Us...>, Ts...> {
+    using type = std::tuple<Us..., Ts...>;
+};
+
+
+// aliasing to append_t
+template <typename Tuple, typename... Ts>
+using append_t = typename append<Tuple, Ts...>::type;
+
+
 int main() {
-  std::cout << "Advance tests main()\n";
+  
+   using L1 = std::tuple<int>;
+   using L2 = append_t<L1, double, char>; 
 
   return 0;
 }
